101298e5a91e941f81c7f2422a5ef80a10cbf552
Gaurav Kumar <forcewithgaurav@gmail.com>
2024-09-14 12:33:49 +0530

    Added Active Rounte Indicator

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,20 +1,49 @@
+<script lang="ts">
+    import type { NavItem } from "../types";
+    import { page } from "$app/stores";
 
-<div class="container bg-blue-500 w-full h-full p-4">
+    let menuItems: NavItem[] = [
+        {
+            label: "Random Users",
+            url: "/randomusers"
+        },
+        {
+            label: "Books",
+            url: "/books",
+        },
+        {
+            label: "Meals",
+            url: "/meals",
+        },
+        {
+            label: "Stocks",
+            url: "/stocks"
+        }
+    ];
+
+    $effect(() => {
+        console.log($page);
+    });
+</script>
+<div class="container bg-blue-600 w-full h-full p-4">
     <h1 class="text-2xl text-white font-bold">FreeApi.app</h1>
-    <div class="container">
+    <div class="container mt-4">
         <ul class="list-none">
-            <li class="p-2 text-white">
-                <a href="/randomusers">Random Users</a>
-            </li>
-            <li class="p-2 text-white">
-                <a href="/books">Books</a>
-            </li>
-            <li class="p-2 text-white">
-                <a href="/meals">Meals</a>
-            </li>
-            <li class="p-2 text-white">
-                <a href="/stocks">Stocks</a>
-            </li>
+            {#each menuItems as menuItem}
+                <li class="mb-2">
+                    <a href={menuItem.url}>
+                        <div class="px-4 py-2 text-white hover:bg-blue-500 rounded-md cursor-pointer" class:active={$page.route.id === menuItem.url}>
+                            {menuItem.label}
+                        </div>
+                    </a>
+                </li>
+            {/each}
         </ul>
     </div>
 </div>
+
+<style>
+    li .active {
+        background-color: rgb(0, 78, 172);
+    }
+</style>
